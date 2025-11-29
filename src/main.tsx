2d3d00b3401124433366597d8bbf4e080bd3fd58
Firoz Khan <firozkhan907979@gmail.com>
2025-11-29 12:03:44 +0530

    fix-3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-
+import App from './App';
 import './index.css'
 
 const rootElement = document.getElementById('root');
@@ -12,6 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <h1>hi</h1>
+    <App />
   </React.StrictMode>
 );
